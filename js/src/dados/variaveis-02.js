9bac575501282b529cc9bbf149b434649bac2d93
isabelaCosta95 <isabelaovc95@gmail.com>
2025-02-04 22:08:07 -0300

    Variaveis 02

---
+++ js/src/dados/variaveis-02.js
@@ -0,0 +1,15 @@
+const x = 10;
+const y = 20;
+
+
+const resultadoDaSoma = (x + y);
+
+// Mesma coisa:
+console.log(x + y); 
+console.log(resultadoDaSoma); 
+
+// Não é possível alterar o valor da constante 
+// x = x + 10;
+
+const xAlterado = x + 10;
+console.log(x , xAlterado,  y);
