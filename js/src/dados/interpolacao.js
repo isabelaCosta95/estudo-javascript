a85326aeb2c5eba93cae5fd2d043c860590e8802
isabelaCosta95 <isabelaovc95@gmail.com>
2025-02-04 23:01:13 -0300

    interpolacao

---
+++ js/src/dados/interpolacao.js
@@ -0,0 +1,6 @@
+const nome = 'Isabela';
+const idade = 20;
+const cidade = 'Presidente Prudente';
+
+const frase = `Olá! Meu nome é ${nome}, tenho ${idade} anos e moro em ${cidade}`;
+console.log(frase);
