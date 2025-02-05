22324aa5681165bbb97615515c6b9ebbf0e9c0a5
isabelaCosta95 <isabelaovc95@gmail.com>
2025-02-05 13:04:54 -0300

    if e else

---
+++ js/src/controle/if-02.js
@@ -0,0 +1,8 @@
+let fome = true;
+let boloMagico = false;
+
+if (fome && boloMagico){
+    console.log('Recuperou energia!');
+} else{
+    console.log('Não come o bolo e continua seu caminho!');
+}
