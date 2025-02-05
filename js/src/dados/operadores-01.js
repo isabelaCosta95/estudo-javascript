24e9ae77602c669d0f2e10ebd15f3fc4f2d01c19
isabelaCosta95 <isabelaovc95@gmail.com>
2025-02-04 21:56:26 -0300

    Operadores-01

---
+++ js/src/dados/operadores-01.js
@@ -0,0 +1,29 @@
+// Operadores Aritiméticos
+
+// Usando valores literais
+console.log(10 + 5);
+console.log(10 - 5);
+console.log(10 * 5);
+console.log(10 / 5);
+console.log(10 % 5);
+
+// Usando variáveis
+let x = 10;
+let y = 5;
+
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+
+// Incremento e Decremento
+let z = 10;
+
+console.log(z++); // Primeiro mostrou no console, e depois incrementou 
+console.log(++z); // Primeiro incrementou, e depois mostrou no console
+console.log(z);
+
+console.log(z--);
+console.log(--z);
+console.log(z);
