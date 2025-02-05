cd591efcca57dc966f729c657f074151a33098f5
isabelaCosta95 <isabelaovc95@gmail.com>
2025-02-05 17:16:26 -0300

    parâmetro de resto

---
+++ js/src/funcoes/basico-05.js
@@ -0,0 +1,12 @@
+// ... trata como se fosse uma lista de elementos
+
+function somarTodos(...numeros){
+    let total = 0;
+    for (let n of numeros){
+        total += n;
+    }
+    return total;
+}
+
+console.log(somarTodos(1, 2, 3, 4, 5));
+console.log(somarTodos(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
