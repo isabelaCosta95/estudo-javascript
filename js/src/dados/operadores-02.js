21c51ed76580f4694cab7d80ce588714d6824d4a
isabelaCosta95 <isabelaovc95@gmail.com>
2025-02-04 22:43:01 -0300

    Operadores 02

---
+++ js/src/dados/operadores-02.js
@@ -0,0 +1,25 @@
+// Operadores relacionais
+// Retorna um booleano
+
+// Usando valores literais
+console.log(10 > 5);
+console.log(10 < 5);
+console.log(10 >= 5);
+console.log(10 <= 5);
+console.log(10 == 5);
+console.log(10 != 5);
+console.log(10 == '10'); 
+console.log(10 === '10'); // comparação estrita 
+
+//Usando variáveis
+let x = 10;
+let y = 5;
+
+console.log(x > y);
+console.log(x < y);
+console.log(x >= y);
+console.log(x <= y);
+console.log(x == y);
+console.log(x != y);
+console.log(x == 'x');
+console.log(x === 'x');
