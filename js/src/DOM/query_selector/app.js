88982f9c239442c54c601bb3c5953f6481116907
isabelaCosta95 <isabelaovc95@gmail.com>
2025-02-13 16:19:15 -0300

    Query Selector e Query Selector All

---
+++ js/src/DOM/query_selector/app.js
@@ -0,0 +1,11 @@
+let id = document.querySelector('#segundo');
+console.log(id);
+
+let div = document.querySelectorAll('div');
+console.log(div);
+
+let classe = document.querySelectorAll('.primeira-div');
+console.log(classe);
+
+let last = document.querySelector('ul > li:last-child');
+console.log(last);
