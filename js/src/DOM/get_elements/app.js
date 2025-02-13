4d330f9ad66e9850e17e1ae2e395681ecbc4f992
isabelaCosta95 <isabelaovc95@gmail.com>
2025-02-13 12:05:50 -0300

    ElementsByName como usar

--- js/src/DOM/get_elements/app.js
+++ js/src/DOM/get_elements/app.js
@@ -3,3 +3,5 @@ console.log(document.getElementById('titulo').innerText);
 document.getElementById('titulo').innerText = "Novo Título";
 
 document.getElementById('texto1').innerText = "Novo Parágrafo";
+
+document.getElementsByName('text_usuario')[0].value = "Isabela";
