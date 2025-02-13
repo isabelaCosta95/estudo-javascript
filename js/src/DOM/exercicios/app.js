96aa33687bf5f94eea4bf895b676403022085fde
isabelaCosta95 <isabelaovc95@gmail.com>
2025-02-13 16:39:05 -0300

    Treinando o Inner

---
+++ js/src/DOM/exercicios/app.js
@@ -0,0 +1,18 @@
+let elemento = document.querySelector("#titulo");
+console.log(elemento);
+
+let div = document.querySelectorAll("div");
+console.log(div);
+
+let cor = document.querySelector("#titulo > i > u");
+cor.style.color = "red";
+cor.style.background = "blue";
+console.log(cor);
+
+let classe = document.querySelector(".noticia > h1");
+console.log(classe.innerText); // Não aparece as tags dentro, ignora o HTML, só mostra o texto
+console.log(classe.innerHTML); // Aparece tudo, inclusive as tags dentro
+console.log(classe.outerHTML); // Aparece o elemento inteiro, inclusive o h1
+
+classe.innerText = "Novo Texto";
+classe.innerHTML = "Novo <i> Texto </i>";
